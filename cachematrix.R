741fefd9463287f9ec629672eefb625efe57f9ab
PaulCourier <30833512+PaulCourier@users.noreply.github.com>
2017-08-18 11:23:11 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,19 +6,22 @@
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
-    x <<- y
+    x <<- y   ## value of matrix in parent environment
     m <<- NULL
   }
   get <- function() x
-  setsolve <- function(solve) m <<- solve
+  setsolve <- function(solve) m <<- solve   ## assigns value of inv in parent environment
   getsolve <- function() m
-  list(set = set, get = get,
+  list(set = set, get = get,    ## you need this in order to refer to the functions with the $ operator
        setsolve= setsolve,
        getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated ,
+## then cacheSolve will retrieve the inverse directly from the cache
+
 
 cacheSolve <- function(x, ...) {
   m <- x$getsolve()
